38c75296fc1ed2dd77f7fd699010cfaf29a68bce
bittencourt-lucas <lucascafieiro@hotmail.com>
2021-04-13 08:24:06 -0300

    First commit, API ready for usage

---
+++ weather-api/src/server.js
@@ -0,0 +1,49 @@
+import 'dotenv/config';
+
+import express from 'express';
+
+import geocode from './utils/geocode';
+import forecast from './utils/forecast';
+
+const app = express();
+const port = 3011;
+
+app.use(express.json());
+
+app.get('/weather', (req, res) => {
+  const { address } = req.query;
+
+  if (!address) {
+    return res.json({
+      error: 'Address must be provided',
+    });
+  }
+
+  geocode(address, (geocodeError, { location, latitude, longitude } = {}) => {
+    if (geocodeError) {
+      return res.json({
+        error: geocodeError,
+      });
+    }
+
+    forecast(
+      latitude,
+      longitude,
+      (err, { summary, temperature, precipProbability }) => {
+        if (err) {
+          return res.json({
+            err,
+          });
+        }
+        return res.json({
+          location,
+          summary,
+          temperature,
+          precipProbability,
+        });
+      },
+    );
+  });
+});
+
+app.listen(port, () => {});
---
+++ weather-api/src/utils/forecast.js
@@ -0,0 +1,22 @@
+import request from 'request';
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.darksky.net/forecast/${
+    process.env.FORECAST_ACCESS_TOKEN
+  }/${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`;
+  request({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback('Weather service is unavailable', undefined);
+    } else if (body.error) {
+      callback(`${body.error}.`, undefined);
+    } else {
+      callback(undefined, {
+        summary: body.daily.data[0].summary,
+        temperature: body.currently.temperature,
+        precipProbability: body.currently.precipProbability,
+      });
+    }
+  });
+};
+
+export default forecast;
